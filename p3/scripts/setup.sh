b8601579524df1732e9596d6fde1432b3e38cbb0
aakhrif <ayoube_akhrif2015@outlook.fr>
2025-07-13 15:07:21 +0100

    v2

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -13,12 +13,23 @@ sudo kubectl create namespace dev
 # apply deployements to the dedicated namespaces
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-# expose port to access the argoCD UI
-kubectl port-forward svc/argocd-server -n argocd 8443:443 > /dev/null 2>&1 &
+# kubectl port-forward $POD_NAME -n dev 8888:8888 > /dev/null 2>&1 &
+POD_NAME=$(kubectl get pods -n "dev" --no-headers | grep "^app" | awk '{print $1}' | head -n 1)
 
 # get secret to access the UI
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
 
+# setup argocd app
+argocd login localhost:8443
 
+argocd app create developement \
+  --repo https://github.com/N1ghtm4reee/Inception-of-Things \
+  --path p3/confs \
+  --dest-server https://kubernetes.default.svc \
+  --dest-namespace dev
 
+argocd app sync developement
 
+# expose app port
+kubectl port-forward $POD_NAME -n dev 8888:8888 > /dev/null 2>&1 &
+kubectl port-forward svc/argocd-server -n argocd 8443:443 > /dev/null 2>&1 &
